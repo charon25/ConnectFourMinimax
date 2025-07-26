b7dde2f949d4e9a1ebe0edc2c4163284f206e077
Paul Kern <paul.kern.fr@gmail.com>
2025-07-26 11:29:21 +0200

    feat (game) : created Main class

--- src/main/java/org/minimax/Main.java
+++ src/main/java/org/minimax/Main.java
@@ -1,8 +1,34 @@
 package org.minimax;
 
+import org.minimax.player.PlayerType;
+
+import static org.minimax.Constants.IN;
+
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Player types: ");
+		for (final PlayerType playerType : PlayerType.values()) {
+			System.out.println("- " + playerType.ordinal() + " : " + playerType);
+		}
 
+		System.out.print("\nPlayer count: ");
+		final int playerCount = Integer.parseInt(IN.nextLine());
+
+		final PlayerType[] players = new PlayerType[playerCount];
+		for (int i = 0; i < playerCount; i++) {
+			System.out.print("Player " + (i + 1) + ": ");
+			players[i] = getPlayerType(IN.nextLine());
+		}
+
+		Game.play(false, players);
+	}
+
+	private static PlayerType getPlayerType(final String input) {
+		try {
+			return PlayerType.values()[Integer.parseInt(input)];
+		} catch (final Exception e) {
+			return PlayerType.valueOf(input.strip().toUpperCase());
+		}
 	}
 }
